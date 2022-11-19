1311eb649d02d5269480b4b451f4e3005f02df5a
Laurence Dougal Myers <laurencedougalmyers@gmail.com>
2022-11-19 14:55:31 +1100

    First attempt to add support for stdin/stdout

--- main.cpp
+++ main.cpp
@@ -6,12 +6,19 @@
 
 #include <windows.h>
 
+#else
+
+# Needed for shouldReadFromStdIn()
+#include <stdio.h>
+#include <unistd.h>
+
 #endif
 
 #include <nowide/args.hpp>
 #include <nowide/iostream.hpp>
 #include <nowide/convert.hpp>
 #include <inja/inja.hpp>
+#include <io.h>
 
 enum class ExitCode : uint8_t {
     Okay,
@@ -32,6 +39,14 @@ boolean isExistingFile(std::filesystem::path &path) {
     return true;
 }
 
+boolean shouldReadFromStdIn() {
+#ifdef WIN32
+    return !_isatty(_fileno(stdin));
+#else
+    return !isatty(fileno(stdin)); // TODO: verify this implementation
+#endif
+}
+
 int main(int argc, char *argv[]) {
     try {
         nowide::args args(argc, argv);
@@ -40,25 +55,44 @@ int main(int argc, char *argv[]) {
         SetConsoleCP(65001); // UTF-8 on Windows
 #endif
 
-        if (argc < 3) {
+        /*
+         * - Check if stdin is a tty.
+         * - If it is, read both input and output args.
+         * - If it's not, treat stdin as input and stdout as output.
+         */
+        if (shouldReadFromStdIn()) {
+            /**
+             * Load the whole input stream into memory. Inja only works with strings.
+             */
+            std::string templateString {};
+            std::cin >> templateString;
+
+            /**
+             * Render the template to a string.
+             */
+            inja::Environment env;
+            inja::json data; // dummy data
+            inja::Template aTemplate = env.parse_template(templateString);
+            std::string result = env.render(templateString, data);
+            nowide::cout << result << std::endl;
+        } else if (argc < 3) {
             nowide::cerr << "Please pass the input and output file paths.";
             return static_cast<int>(ExitCode::MissingArg);
+        } else {
+            std::filesystem::path inputPath {nowide::widen(argv[1])};
+            std::filesystem::path outputPath {nowide::widen(argv[2])};
+
+            if (!isExistingFile(inputPath)) {
+                nowide::cerr << "Input file not found.";
+                return static_cast<int>(ExitCode::InvalidArg);
+            }
+
+            std::filesystem::create_directories(outputPath.parent_path());
+
+            inja::Environment env;
+            inja::json data; // dummy data
+            env.write(inputPath.string(), data, outputPath.string());
         }
-
-        std::filesystem::path inputPath {nowide::widen(argv[1])};
-        std::filesystem::path outputPath {nowide::widen(argv[2])};
-
-        if (!isExistingFile(inputPath)) {
-            nowide::cerr << "Input file not found.";
-            return static_cast<int>(ExitCode::InvalidArg);
-        }
-
-        std::filesystem::create_directories(outputPath.parent_path());
-
-        inja::Environment env;
-        inja::json data; // dummy data
-
-        env.write(inputPath.string(), data, outputPath.string());
 
         return static_cast<int>(ExitCode::Okay);
     } catch (std::exception &err) {
